36813b30e41991ad37bef868c597f916add38cb4
Sudhanshu-1508 <sudhanshukulkarni55@gmail.com>
2023-03-08 13:00:32 +0530

    created a navbar

---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='nav'>
+        <a href='/' className='site-title'>Site name</a>
+        <ul>
+            <li className='active'>
+                <a href='/pricing'>Pricing</a>
+            </li>
+            <li>
+                <a href='/about'>About</a>
+            </li>
+        </ul>  
+    </nav>
+  )
+}
+
+export default Navbar
